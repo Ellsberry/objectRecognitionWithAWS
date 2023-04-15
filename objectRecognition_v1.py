fb3da1e7092a68407aba59ba67aba108840d6852
Anthony081111 <ajh081111@gmail.com>
2023-04-15 11:08:31 -0400

    Added labels(but some say rghnripn)

--- objectRecognition_v1.py
+++ objectRecognition_v1.py
@@ -8,9 +8,10 @@ import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 import io
 import json
+from pprint import pprint
 
 # Get the access codes to run the AWS  boto3 and rekognition programs
-file_df = pd.read_csv('recognition_v1_credentials.csv')
+file_df = pd.read_csv('recognition_v1_accessKeys.csv')
 access_key_id = file_df['Access key ID'].loc[file_df.index[0]]
 secret_access_key = file_df['Secret access key'].loc[file_df.index[0]]
 # print(f"access key = {access_key_id}   and secret access key =  {secret_access_key}")
@@ -28,13 +29,14 @@ with open(photo, 'rb') as image_file:
     source_bytes = image_file.read()
 
 detect_objects = client.detect_labels(Image={'Bytes': source_bytes})
-print(detect_objects)
+# print(detect_objects)
 
 pd.options.display.max_columns = 10
 detected_json_data_df = pd.json_normalize(detect_objects['Labels'])
+# print("The next print uses pprint with depth = 8")
+pprint(detect_objects, depth=8)
 
 print(detected_json_data_df[0:200])
-
 
 
 image = Image.open(io.BytesIO(source_bytes))
@@ -43,7 +45,6 @@ draw = ImageDraw.Draw(image)
 for label in detect_objects['Labels']:
     print(label['Name'])
     print('confidences: ', label['Confidence'])
-
 
     for instances in label['Instances']:
         if 'BoundingBox' in instances:
@@ -62,8 +63,16 @@ for label in detect_objects['Labels']:
                         (left, top + height),
                         (left, top)
                     )
-
-
             draw.line(points, width=5, fill= '#69f5d9')
 
+            points = (
+                (left, top - 50),
+                (left + width, top - 50),
+                (left + width, top),
+                (left, top),
+                (left, top - 50)
+            )
+
+            draw.line(points, width=5, fill= '#69f5d9')
+            draw.text((left + 10, top - 30), label["Name"])
 image.show()
