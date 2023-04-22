d3b9b524cb0fcedb536cd91d29a9f48569536189
Anthony081111 <ajh081111@gmail.com>
2023-04-22 11:36:18 -0400

    fixed and added lists

--- objectRecognition_v1.py
+++ objectRecognition_v1.py
@@ -19,15 +19,17 @@ secret_access_key = file_df['Secret access key'].loc[file_df.index[0]]
 client = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id=access_key_id,
                       aws_secret_access_key=secret_access_key)
 
-photo = "group_of_animals.jpg"
-# photo = "five_people.jpg"
-# photo = "michel_anthony.jpg"
-# photo = "anthony.jpg"
-# photo = "anthony_steve.jpg"
-# photo = "michael_anthony_lisa.jpg"
-# photo = "michel.jpg"
+photo = [
+    "totally_not_an_elephant.jpg",
+    "group_of_animals.jpg",
+    "five_people.jpg",
+    "michel_anthony.jpg",
+    "anthony.jpg",
+    "anthony_steve.jpg",
+    "michael_anthony_lisa.jpg",
+    "michel.jpg"]
 
-with open(photo, 'rb') as image_file:
+with open(photo[0], 'rb') as image_file:
     source_bytes = image_file.read()
 
 detect_objects = client.detect_labels(Image={'Bytes': source_bytes})
@@ -36,46 +38,57 @@ detect_objects = client.detect_labels(Image={'Bytes': source_bytes})
 pd.options.display.max_columns = 10
 detected_json_data_df = pd.json_normalize(detect_objects['Labels'])
 # print("The next print uses pprint with depth = 8")
-pprint(detect_objects, depth=8)
+# pprint(detect_objects, depth=8)
 
 # print(detected_json_data_df[0:200])
 
+label_list = []
 boxlist = []
 image = Image.open(io.BytesIO(source_bytes))
 draw = ImageDraw.Draw(image)
 
 for label in detect_objects['Labels']:
-    print(label['Name'])
-    print('confidences: ', label['Confidence'])
+    # print(label['Name'])
+    # print('confidences: ', label['Confidence'])
 
     for instances in label['Instances']:
         if 'BoundingBox' in instances:
+            if label["Name"] == "Elephant":
 
-            box = instances['BoundingBox']
+                box = instances['BoundingBox']
 
-            left = image.width * box['Left']
-            top = image.height * box['Top']
-            width = image.width * box['Width']
-            height = image.height * box['Height']
-            points = (
-                        (left, top),
-                        (left + width, top),
-                        (left + width, top + height),
-                        (left, top + height),
-                        (left, top)
-                    )
-            draw.line(points, width=5, fill= '#69f5d9')
-
-            points = (
-                (left, top - 50),
-                (left + width, top - 50),
-                (left + width, top),
-                (left, top),
-                (left, top - 50)
-            )
-            if box not in boxlist:
+                left = image.width * box['Left']
+                top = image.height * box['Top']
+                width = image.width * box['Width']
+                height = image.height * box['Height']
+                points = (
+                            (left, top),
+                            (left + width, top),
+                            (left + width, top + height),
+                            (left, top + height),
+                            (left, top)
+                        )
                 draw.line(points, width=5, fill= '#69f5d9')
-                draw.text((left + 10, top - 30), label["Name"], fill="#000000")
-                boxlist.append(box)
-                print(label["Name"], "\n", boxlist)
+
+                points = (
+                    (left, top - 50),
+                    (left + width, top - 50),
+                    (left + width, top),
+                    (left, top),
+                    (left, top - 50)
+                )
+                if box not in boxlist:
+                    draw.line(points, width=5, fill= '#69f5d9')
+                    draw.text((left + 10, top - 30), label["Name"], fill="#000000")
+                    box_name = label["Name"]
+                    boxlist.append((box_name, box))
+                    box_confidence = label["Confidence"]
+                    label_list.append((box_name, box_confidence))
+                    # print(boxlist)
 image.show()
+
+for item in boxlist:
+    print(item, "\n")
+label_list.sort()
+for thing in label_list:
+    print(thing, "\n")
